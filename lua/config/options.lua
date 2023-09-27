190152645777b31ac585acfb6b7b74bfa33ee3c2
Iain Buchanan <ibuchanan@replicant.ai>
2023-09-27 08:59:46 -0400

    revamp via lazyvim

---
+++ lua/config/options.lua
@@ -0,0 +1,10 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- absolute line numbers!
+vim.opt.relativenumber = false
+
+-- a couple of theme options
+vim.g.everforest_background = "soft"
+vim.g.everforest_better_performance = true
